5f6c50a106a1e881b2622413ad31dc3c93a068ce
anonimvt <75793665+anonimvt@users.noreply.github.com>
2023-05-27 15:05:19 +0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 # Task number 34
-def rhythm(str):
+def vinnipux(str):
     str = str.split()
     list = []
     for word in str:
@@ -12,7 +12,7 @@ def rhythm(str):
 
 print('Введите: пара-ра-рам рам-пам-папам па-ра-па-дам')
 str = input()
-if rhythm(str):
+if vinnipux(str):
     print('Парам пам-пам')
 else:
     print('Пам парам')
@@ -22,9 +22,9 @@ else:
 # Task number 36
 
 
-def print_operation_table(operation, num_rows=6, num_columns=6):
+def oper(operation, num_rows=6, num_columns=6):
     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
     for i in a:
         print(*[f"{x:>3}" for x in i])
 
-print_operation_table(lambda x, y: x * y)
+oper(lambda x, y: x * y)
